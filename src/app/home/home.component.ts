e88dc63bd3d9e73972189cac97056e9796b59dc2
Kevin <kwburnett@gmail.com>
2020-02-21 20:55:25 +0100

    starting work on initial home screen; need to add the graph next with test data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Page, isAndroid } from 'tns-core-modules/ui/page/page';
 
 @Component({
   selector: 'nepa-home',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private page: Page) {
+    if (isAndroid) {
+      this.page.actionBarHidden = true;
+    }
+  }
 
   ngOnInit(): void {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { registerElement } from 'nativescript-angular/element-registry';
+import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
+import { ShadowedLabel } from 'nativescript-shadowed-label';
+import { AppModule } from './app/app.module';
 
-import { AppModule } from "./app/app.module";
+registerElement('ShadowedLabel', () => ShadowedLabel);
 
 // A traditional NativeScript application starts by initializing global objects,
 // setting up global CSS rules, creating, and navigating to the main page.
