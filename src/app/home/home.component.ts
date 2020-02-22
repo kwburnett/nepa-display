207882ebe76779d713fa3471fe364c0f77bbf5a4
Kevin <kwburnett@gmail.com>
2020-02-22 17:12:23 +0100

    updating the home component to adjust depending on whether power is on or not

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { isAndroid, Page } from 'tns-core-modules/ui/page/page';
 import { PowerService } from './power.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'nepa-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public powerData$: Observable<number[]>;
+  public isPowerOn: boolean = false;
+
+  private subscriptions: Subscription[] = [];
+
   constructor(private page: Page, private powerService: PowerService) {
     if (isAndroid) {
       this.page.actionBarHidden = true;
@@ -18,5 +22,13 @@ export class HomeComponent implements OnInit {
 
   public ngOnInit(): void {
     this.powerData$ = this.powerService.getPowerData();
+
+    this.subscriptions.push(this.powerData$.subscribe((powerData: number[]): void => {
+      this.isPowerOn = powerData[powerData.length - 1] !== 0;
+    }));
+  }
+  
+  public ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
 }
