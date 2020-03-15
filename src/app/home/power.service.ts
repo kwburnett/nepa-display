bb56274f0e62a193a50fcacb0e63099d641cba90
Kevin <kwburnett@gmail.com>
2020-03-15 20:07:59 +0100

    fixed problem with interruptions display

--- src/app/home/power.service.ts
+++ src/app/home/power.service.ts
@@ -1,233 +1,171 @@
 import { Injectable } from '@angular/core';
-import {
-  BehaviorSubject,
-  combineLatest,
-  interval,
-  Observable,
-  of,
-  Subscription
-} from 'rxjs';
+import { BehaviorSubject, combineLatest, interval, Observable, of, Subscription } from 'rxjs';
 import { flatMap, map } from 'rxjs/operators';
 import { IPowerData } from '../../model/i-power-data';
 import { IRecentSwitch } from '../../model/i-recent-switch';
 import { isPowerOff, isPowerOn } from '../utils';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class PowerService {
-  private voltageData: IPowerData[] = [];
-  private voltageSwitches: IPowerData[] = [];
-  private nextDataIndexToGet: number;
-  private minimumNumberOfMinutesToNotConstituteInterruption: number = 15;
-  private frequencyOfDataCheckPerMinute = 60;
-  private dataSubscription: Subscription;
-  private returnedPowerData: BehaviorSubject<
-    IPowerData[]
-  > = new BehaviorSubject([]);
-  private returnedPowerSwitches: BehaviorSubject<
-    IPowerData[]
-  > = new BehaviorSubject([]);
+	private _voltageData: IPowerData[] = [];
+	private _voltageSwitches: IPowerData[] = [];
+	private _nextDataIndexToGet: number;
+	private _minimumNumberOfMinutesToNotConstituteInterruption: number = 15;
+	private _frequencyOfDataCheckPerMinute = 60;
+	private _dataSubscription: Subscription;
+	private _returnedPowerData: BehaviorSubject<IPowerData[]> = new BehaviorSubject([]);
+	private _returnedPowerSwitches: BehaviorSubject<IPowerData[]> = new BehaviorSubject([]);
 
-  private init(): void {
-    this.seedVoltageData();
-    this.setVoltageSwitches();
-  }
+	private _init(): void {
+		this._seedVoltageData();
+		this._setVoltageSwitches();
+	}
 
-  private setVoltageSwitches(): void {
-    let powerWasOn: boolean = isPowerOn(this.voltageData[0]);
-    for (let voltageDataPoint of this.voltageData) {
-      if (
-        (isPowerOff(voltageDataPoint) && powerWasOn) ||
-        (isPowerOn(voltageDataPoint) && !powerWasOn)
-      ) {
-        this.voltageSwitches.push(voltageDataPoint);
-        powerWasOn = isPowerOn(voltageDataPoint);
-      }
-    }
-    this.returnedPowerSwitches.next(this.voltageSwitches);
-  }
+	private _setVoltageSwitches(): void {
+		let powerWasOn: boolean = isPowerOn(this._voltageData[0]);
+		for (let voltageDataPoint of this._voltageData) {
+			if ((isPowerOff(voltageDataPoint) && powerWasOn) || (isPowerOn(voltageDataPoint) && !powerWasOn)) {
+				this._voltageSwitches.push(voltageDataPoint);
+				powerWasOn = isPowerOn(voltageDataPoint);
+			}
+		}
+		this._returnedPowerSwitches.next(this._voltageSwitches);
+	}
 
-  private seedVoltageData(): void {
-    try {
-      const voltageData: number[] = require('../../json/voltage-data.json');
-      const numberOfDataPoints: number = voltageData.length;
-      const tempSeedIndex = Math.floor(Math.random() * numberOfDataPoints);
-      // Re-shuffle the data so it's ordered according to the seed index
-      voltageData.unshift(...voltageData.splice(tempSeedIndex));
+	private _seedVoltageData(): void {
+		try {
+			const voltageData: number[] = [...(require('../../json/voltage-data.json') as number[])];
+			const numberOfDataPoints: number = voltageData.length;
+			// const tempSeedIndex = Math.floor(Math.random() * numberOfDataPoints);
+			const tempSeedIndex = (Math.floor(numberOfDataPoints / 2) + 3658) % numberOfDataPoints;
+			// Re-shuffle the data so it's ordered according to the seed index
+			voltageData.unshift(...voltageData.splice(tempSeedIndex));
 
-      const nowIndex = Math.floor(numberOfDataPoints / 2);
-      const dateNow = new Date();
-      // Start numbering the data into the future
-      for (let i = nowIndex; i < numberOfDataPoints; i++) {
-        this.voltageData.push({
-          time: dateNow.getTime() + (i - nowIndex) * 60000,
-          voltage: voltageData[i]
-        });
-      }
-      // Number the data back into the past
-      for (let i = nowIndex - 1; i >= 0; i--) {
-        this.voltageData.unshift({
-          time: dateNow.getTime() - (nowIndex - i) * 60000,
-          voltage: voltageData[i]
-        });
-      }
-      this.nextDataIndexToGet = nowIndex + 1;
-    } catch (err) {
-      console.log('There was an error initializing the power data.');
-      throw err;
-    }
-  }
+			const nowIndex = Math.floor(numberOfDataPoints / 2);
+			const dateNow = new Date();
+			// Start numbering the data into the future
+			this._voltageData = voltageData.map(
+				(voltage: number, index: number): IPowerData => {
+					return {
+						time: dateNow.getTime() - (nowIndex - index) * 60000,
+						voltage,
+					};
+				},
+			);
+			this._nextDataIndexToGet = nowIndex + 1;
+		} catch (err) {
+			console.log('There was an error initializing the power data.');
+			throw err;
+		}
+	}
 
-  private tempGetPowerData(): Observable<IPowerData[]> {
-    this.returnedPowerData.next(
-      this.voltageData.slice(
-        this.nextDataIndexToGet - 4 * 60,
-        this.nextDataIndexToGet
-      )
-    );
-    return this.returnedPowerData.asObservable();
-  }
+	private _tempGetPowerData(): Observable<IPowerData[]> {
+		this._returnedPowerData.next(this._voltageData.slice(this._nextDataIndexToGet - 4 * 60, this._nextDataIndexToGet));
+		return this._returnedPowerData.asObservable();
+	}
 
-  private tempGetSubsequentPowerData(): Observable<IPowerData[]> {
-    return of(
-      this.voltageData.slice(this.nextDataIndexToGet, ++this.nextDataIndexToGet)
-    );
-  }
+	private _tempGetSubsequentPowerData(): Observable<IPowerData[]> {
+		return of(this._voltageData.slice(this._nextDataIndexToGet, ++this._nextDataIndexToGet));
+	}
 
-  private tempGetPowerSwitches(
-    timeOfLatestDataPointDisplayed: number
-  ): Observable<IPowerData[]> {
-    return combineLatest(
-      this.returnedPowerData.asObservable(),
-      this.returnedPowerSwitches.asObservable()
-    ).pipe(
-      map(
-        ([powerData, powerDataSwitches]: [
-          IPowerData[],
-          IPowerData[]
-        ]): IPowerData[] => {
-          const indexOfChangePastLatest: number = powerDataSwitches.findIndex(
-            (powerDataSwitchDataPoint: IPowerData): boolean =>
-              powerDataSwitchDataPoint.time > timeOfLatestDataPointDisplayed
-          );
-          const timeOfMostRecentDataPointDisplayed = this.voltageData[
-            this.nextDataIndexToGet - 1
-          ].time;
-          const indexOfChangePastRecent: number = powerDataSwitches.findIndex(
-            (powerDataSwitchDataPoint: IPowerData): boolean =>
-              powerDataSwitchDataPoint.time > timeOfMostRecentDataPointDisplayed
-          );
-          return powerDataSwitches.slice(
-            indexOfChangePastLatest - 1,
-            indexOfChangePastRecent
-          );
-        }
-      )
-    );
-  }
+	private _tempGetPowerSwitches(timeOfLatestDataPointDisplayed: number): Observable<IPowerData[]> {
+		return combineLatest(this._returnedPowerData.asObservable(), this._returnedPowerSwitches.asObservable()).pipe(
+			map(([powerData, powerDataSwitches]: [IPowerData[], IPowerData[]]): IPowerData[] => {
+				const indexOfChangePastLatest: number = powerDataSwitches.findIndex(
+					(powerDataSwitchDataPoint: IPowerData): boolean =>
+						powerDataSwitchDataPoint.time > timeOfLatestDataPointDisplayed,
+				);
+				const timeOfMostRecentDataPointDisplayed = this._voltageData[this._nextDataIndexToGet - 1].time;
+				const indexOfChangePastRecent: number = powerDataSwitches.findIndex(
+					(powerDataSwitchDataPoint: IPowerData): boolean =>
+						powerDataSwitchDataPoint.time > timeOfMostRecentDataPointDisplayed,
+				);
+				return powerDataSwitches.slice(indexOfChangePastLatest - 1, indexOfChangePastRecent);
+			}),
+		);
+	}
 
-  constructor() {
-    this.init();
-  }
+	constructor() {
+		this._init();
+	}
 
-  public getPowerData(): Observable<IPowerData[]> {
-    return this.tempGetPowerData();
-  }
+	public getPowerData(): Observable<IPowerData[]> {
+		return this._tempGetPowerData();
+	}
 
-  public getMostRecentPowerOutage(): Observable<IRecentSwitch> {
-    return this.tempGetMostRecentPowerSwitch(isPowerOff);
-  }
+	public getMostRecentPowerOutage(): Observable<IRecentSwitch> {
+		return this._tempGetMostRecentPowerSwitch(isPowerOff);
+	}
 
-  public getMostRecentPowerRestoration(): Observable<IRecentSwitch> {
-    return this.tempGetMostRecentPowerSwitch(isPowerOn);
-  }
+	public getMostRecentPowerRestoration(): Observable<IRecentSwitch> {
+		return this._tempGetMostRecentPowerSwitch(isPowerOn);
+	}
 
-  public getPowerSwitches(
-    timeOfLatestDataPointDisplayed: number
-  ): Observable<IPowerData[]> {
-    return this.tempGetPowerSwitches(timeOfLatestDataPointDisplayed);
-  }
+	public getPowerSwitches(timeOfLatestDataPointDisplayed: number): Observable<IPowerData[]> {
+		return this._tempGetPowerSwitches(timeOfLatestDataPointDisplayed);
+	}
 
-  private tempGetMostRecentPowerSwitch(
-    powerCheckFunction: (powerDataPoint: IPowerData) => boolean
-  ): Observable<IRecentSwitch> {
-    let indexToUse = -1;
-    let numberOfInterruptions = 0;
-    for (let i = 1; i < this.nextDataIndexToGet; i++) {
-      if (
-        !powerCheckFunction(this.voltageData[i - 1]) &&
-        powerCheckFunction(this.voltageData[i])
-      ) {
-        const timeOfCurrentMeasurement: number = this.voltageData[i].time;
-        const voltageDataThatMayBeInterruption = this.voltageData.filter(
-          (powerDataPoint: IPowerData): boolean =>
-            powerDataPoint.time >= timeOfCurrentMeasurement &&
-            powerDataPoint.time <
-              timeOfCurrentMeasurement +
-                this.minimumNumberOfMinutesToNotConstituteInterruption
-        );
-        // If all of the next N match the power check, this isn't an interruption
-        if (voltageDataThatMayBeInterruption.every(powerCheckFunction)) {
-          while (
-            this.voltageData[++i].time <
-            timeOfCurrentMeasurement +
-              this.minimumNumberOfMinutesToNotConstituteInterruption
-          ) {}
-          numberOfInterruptions = 0;
-          indexToUse = i;
-        }
-      } else if (
-        powerCheckFunction(this.voltageData[i - 1]) &&
-        !powerCheckFunction(this.voltageData[i]) &&
-        indexToUse > -1
-      ) {
-        const timeOfCurrentMeasurement: number = this.voltageData[i].time;
-        const voltageDataThatMayBeInterruption = this.voltageData.filter(
-          (powerDataPoint: IPowerData): boolean =>
-            powerDataPoint.time >= timeOfCurrentMeasurement &&
-            powerDataPoint.time <
-              timeOfCurrentMeasurement +
-                this.minimumNumberOfMinutesToNotConstituteInterruption
-        );
-        // If any of the next N match the power check, this is an interruption
-        if (voltageDataThatMayBeInterruption.some(powerCheckFunction)) {
-          numberOfInterruptions++;
-          i +=
-            voltageDataThatMayBeInterruption.findIndex(powerCheckFunction) + 1;
-        }
-      }
-    }
-    return of({
-      powerChange: this.voltageData[indexToUse],
-      numberOfInterruptions
-    });
-  }
+	private _tempGetMostRecentPowerSwitch(
+		powerCheckFunction: (powerDataPoint: IPowerData) => boolean,
+	): Observable<IRecentSwitch> {
+		let numberOfInterruptions = 0;
+		const timeOfCurrentData = this._voltageData[this._nextDataIndexToGet - 1].time;
+		const indexOfPowerSwitchBeforeCurrent =
+			this._voltageSwitches.findIndex((voltageSwitch: IPowerData): boolean => voltageSwitch.time > timeOfCurrentData) -
+			1;
+		let powerChange: IPowerData;
+		for (let i = indexOfPowerSwitchBeforeCurrent; i >= 0; i--) {
+			if (!powerCheckFunction(this._voltageSwitches[i])) {
+				continue;
+			}
+			if (
+				this._voltageSwitches[i].time - this._voltageSwitches[i - 1].time >=
+				this._minimumNumberOfMinutesToNotConstituteInterruption * 1000 * 60
+			) {
+				powerChange = this._voltageSwitches[i];
+				break;
+			} else {
+				numberOfInterruptions++;
+			}
+		}
+		return of({
+			powerChange,
+			numberOfInterruptions,
+		});
+	}
 
-  public initiateDataCheck(): void {
-    if (this.dataSubscription) {
-      return;
-    }
-    this.dataSubscription = interval(
-      (60 * 1000) / this.frequencyOfDataCheckPerMinute
-    )
-      .pipe(flatMap(() => this.fetchSubsequentData()))
-      .subscribe((latestPowerDataPoints: IPowerData[]): void => {
-        this.returnedPowerData.next([
-          ...this.returnedPowerData.getValue(),
-          ...latestPowerDataPoints
-        ]);
-      });
-  }
+	public initiateDataCheck(): void {
+		if (this._dataSubscription) {
+			return;
+		}
+		this._dataSubscription = interval((60 * 1000) / this._frequencyOfDataCheckPerMinute)
+			.pipe(flatMap(() => this._fetchSubsequentData()))
+			.subscribe((latestPowerDataPoints: IPowerData[]): void => {
+				this._returnedPowerData.next([...this._returnedPowerData.getValue(), ...latestPowerDataPoints]);
+				// this._returnedPowerData.next([
+				// 	...this._returnedPowerData.getValue(),
+				// 	...latestPowerDataPoints.map(
+				// 		(oldData: IPowerData): IPowerData => {
+				// 			return {
+				// 				time: oldData.time,
+				// 				voltage: undefined,
+				// 			};
+				// 		},
+				// 	),
+				// ]);
+			});
+	}
 
-  public cancelDataCheck(): void {
-    if (this.dataSubscription) {
-      this.dataSubscription.unsubscribe();
-      this.dataSubscription = null;
-    }
-  }
+	public cancelDataCheck(): void {
+		if (this._dataSubscription) {
+			this._dataSubscription.unsubscribe();
+			this._dataSubscription = null;
+		}
+	}
 
-  private fetchSubsequentData(): Observable<IPowerData[]> {
-    return this.tempGetSubsequentPowerData();
-  }
+	private _fetchSubsequentData(): Observable<IPowerData[]> {
+		return this._tempGetSubsequentPowerData();
+	}
 }
--- src/tests/utils.spec.ts
+++ src/tests/utils.spec.ts
@@ -1,29 +1,43 @@
+import { isPowerOff, isPowerOn, isNullOrUndefined } from '../app/utils';
 import { IPowerData } from '../model/i-power-data';
-import { isPowerOff, isPowerOn } from '../app/utils';
 
 describe('utils', () => {
-  const voltageOn: IPowerData = {
-    time: new Date().getTime(),
-    voltage: 231,
-  };
-  const voltageOff: IPowerData = {
-    time: new Date().getTime(),
-    voltage: 0,
-  };
-  const voltageNoData: IPowerData = {
-    time: new Date().getTime(),
-  };
+	const voltageOn: IPowerData = {
+		time: new Date().getTime(),
+		voltage: 231,
+	};
+	const voltageOff: IPowerData = {
+		time: new Date().getTime(),
+		voltage: 0,
+	};
+	const voltageNoData: IPowerData = {
+		time: new Date().getTime(),
+	};
 
-  describe('isPowerOn', () => {
-    it('should return true if not 0', () => {
-      expect(isPowerOn(voltageOn)).toBe(true);
-      expect(isPowerOn(voltageOff)).toBe(false);
-      expect(isPowerOn(voltageNoData)).toBe(false);
-    });
-    it('should return false if 0 or no data', () => {
-      expect(isPowerOff(voltageOn)).toBe(false);
-      expect(isPowerOff(voltageOff)).toBe(true);
-      expect(isPowerOff(voltageNoData)).toBe(true);
-    });
-  });
+	describe('isPowerOn', () => {
+		it('should return true if not 0', () => {
+			expect(isPowerOn(voltageOn)).toBe(true);
+			expect(isPowerOn(voltageOff)).toBe(false);
+			expect(isPowerOn(voltageNoData)).toBe(false);
+		});
+	});
+	describe('isPowerOff', () => {
+		it('should return false if 0 or no data', () => {
+			expect(isPowerOff(voltageOn)).toBe(false);
+			expect(isPowerOff(voltageOff)).toBe(true);
+			expect(isPowerOff(voltageNoData)).toBe(true);
+		});
+	});
+	describe('isNullOrUndefined', () => {
+		it('should return true if null', () => {
+			expect(isNullOrUndefined(null)).toBe(true);
+		});
+		it('should return true if undefined', () => {
+			expect(isNullOrUndefined(undefined)).toBe(true);
+		});
+		it('should return false for falsey values', () => {
+			expect(isNullOrUndefined(0)).toBe(false);
+			expect(isNullOrUndefined(false)).toBe(false);
+		});
+	});
 });
